ba018e9b73bcdbad423ea4a4425bd320f1450f6a
fantasy <fantasy@fantasy-PC>
2013-05-21 19:20:57 +0800

    Version 0.1

--- gen/com/hzh/simplezhkuclassesviewer/R.java
+++ gen/com/hzh/simplezhkuclassesviewer/R.java
@@ -24,18 +24,37 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000b;
+        public static final int course_classes=0x7f080009;
+        public static final int course_classroom=0x7f08000a;
+        public static final int course_name=0x7f080006;
+        public static final int course_teacher=0x7f080007;
+        public static final int course_weeks=0x7f080008;
+        public static final int day_listView=0x7f080000;
+        public static final int day_week=0x7f080004;
+        public static final int each_listView=0x7f080005;
+        public static final int login=0x7f080003;
+        public static final int password=0x7f080002;
+        public static final int stu_id=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_display=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int coursev_view_day=0x7f030002;
+        public static final int coursev_view_each=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int display=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int stu_id=0x7f050003;
+        public static final int stu_psw=0x7f050004;
+        public static final int test=0x7f050005;
+        public static final int title_activity_display=0x7f050006;
     }
     public static final class style {
         /** 
--- src/com/hzh/simplezhkuclassesviewer/MainActivity.java
+++ src/com/hzh/simplezhkuclassesviewer/MainActivity.java
@@ -1,15 +1,57 @@
 package com.hzh.simplezhkuclassesviewer;
 
-import android.os.Bundle;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.hzh.bean.CourseBean;
+import com.hzh.service.MainService;
 
 public class MainActivity extends Activity {
+
+	EditText stuID;
+	EditText stuPsw;
+	MainService mainService = new MainService();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+				.detectDiskReads().detectDiskWrites().detectNetwork()
+				.penaltyLog().build());
+		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+				.detectLeakedSqlLiteObjects()
+				.penaltyLog()
+				.penaltyDeath().build());
+
+		stuID = (EditText) this.findViewById(R.id.stu_id);
+		stuPsw = (EditText) this.findViewById(R.id.password);
+
+		Button btn = (Button) this.findViewById(R.id.login);
+		btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				HashMap<String, List<CourseBean>> data = (HashMap<String, List<CourseBean>>) mainService
+						.downloadAndSave(stuID.getText().toString(), stuPsw
+								.getText().toString());
+				Intent intent = new Intent(MainActivity.this,
+						DisplayActivity.class);
+				intent.putExtra("courses", data);
+				startActivity(intent);
+			}
+
+		});
 	}
 
 	@Override
